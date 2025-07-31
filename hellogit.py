a8d1dfadddaf093de8fe7012549c26b2ec917a9e
Jean Jairo <j.senadortorres@gmail.com>
2025-07-31 11:38:26 -0500

    Hello Git Hub

--- hellogit.py
+++ hellogit.py
@@ -3,3 +3,6 @@ print("This is a simple Python script to demonstrate Git usage.")
 
 
 print("You can modify this file and commit changes to your Git repository.")
+
+
+print("github.com/yourusername/yourrepository for more information.")
